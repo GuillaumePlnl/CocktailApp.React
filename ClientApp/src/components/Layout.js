f1519ec7171031807a0820433432315e19bd6884
Guillaume Planiol <guillaume.planiol@gmail.com>
2021-11-05 16:17:38 +0100

    ThemeContextReact added

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -2,17 +2,43 @@ import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
+import { ThemeContext, themes } from '../ThemeContext/theme-context';
+// import { ThemedButton } from './ThemeContext/themed-button';
+
+
 export class Layout extends Component {
   static displayName = Layout.name;
 
+  state = {
+    context: {
+      themeName: "light",
+      theme: themes.light,
+      toggleTheme: () => {
+        console.log("toggleTheme called")
+        const context = {...this.state.context}
+        context.themeName = this.state.context.themeName === "dark" ? "light" : "dark"
+        context.theme = this.state.context.themeName === "dark" ? themes.light : themes.dark
+        this.setState({context})
+      }
+
+    }
+
+  }
+
   render () {
+    console.log("Layout : state avant : " + this.state.context.theme.background);
     return (
       <div>
-        <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
+        <ThemeContext.Provider value={this.state.context}>
+
+          <NavMenu/>
+
+          <Container>
+            {this.props.children}
+          </Container>
+        </ThemeContext.Provider>
       </div>
     );
-  }
+  } 
 }
+Layout.contextType = ThemeContext;
